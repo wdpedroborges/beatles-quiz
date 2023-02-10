324cb6b25aa911c7fc0b14cda6f36e530fc1598b
wdpedroborges <xpedrostewart@gmail.com>
2023-02-09 23:16:40 -0300

    Correção na escolha dos álbuns

--- script.js
+++ script.js
@@ -1,21 +1,17 @@
+let novoDataset = [{album: "A Hard Day's Night", musicas: ["01 A Hard Day's Night", "02 I Should Have Known Better", "03 If I Fell", "04 I'm Happy Just To Dance With You", "05 And I Love Her", "06 Tell Me Why", "07 Can't Buy Me Love", "08 Any Time At All", "09 I'll Cry Instead", "10 Things We Said Today", "11 When I Get Home", "12 You Can't Do That", "13 I'll Be Back"]}, {album: "Abbey Road", musicas: ["01 Come Together", "02 Something", "03 Maxwell's Silver Hammer", "04 Oh! Darling", "05 Octopus's Garden", "06 I Want You (She's So Heavy)", "07 Here Comes The Sun", "08 Because", "09 You Never Give Me Your Money", "10 Sun King", "11 Mean Mr. Mustard", "12 Polythene Pam", "13 She Came In Through The Bathroom Window", "14 Golden Slumbers", "15 Carry That Weight", "16 The End", "17 Her Majesty"]}, {album: "Beatles For Sale", musicas: ["01 No Reply", "02 I'm A Loser", "03 Baby's In Black", "04 Rock And Roll Music", "05 I'll Follow The Sun", "06 Mr. Moonlight", "07 Medley_ A. Kansas City B. Hey, Hey, Hey, Hey", "08 Eight Days A Week", "09 Words Of Love", "10 Honey Don't", "11 Every Little Thing", "12 I Don't Want To Spoil The Party", "13 What You're Doing", "14 Everybody's Trying To Be My Baby"]}, {album: "Help!", musicas: ["01 Help!", "02 The Night Before", "03 You've Got To Hide Your Love Away", "04 I Need You", "05 Another Girl", "06 You're Going To Lose That Girl", "07 Ticket To Ride", "08 Act Naturally", "09 It's Only Love", "10 You Like Me Too Much", "11 Tell Me What You See", "12 I`ve Just Seen A Face", "13 Yesterday", "14 Dizzy Miss Lizzy"]}, {album: "Let It Be", musicas: ["01 Two Of Us", "02 Dig A Pony", "03 Across The Universe", "04 I Me Mine", "05 Dig It", "06 Let It Be", "07 Maggie Mae", "08 I've Got A Feeling", "09 One After 909", "10 The Long And Winding Road", "11 For You Blue", "12 Get Back"]}, {album: "Magical Mystery Tour", musicas: ["01 Magical Mystery Tour", "02 The Fool On The Hill", "03 Flying", "04 Blue Jay Way", "05 Your Mother Should Know", "06 I Am The Walrus", "07 Hello Goodbye", "08 Strawberry Fields Forever", "09 Penny Lane", "10 Baby You're A Rich Man", "11 All You Need Is Love"]}, {album: "Please Please Me", musicas: ["01 I Saw Her Standing There", "02 Misery", "03 Anna (Go To Him)", "04 Chains", "05 Boys", "06 Ask Me Why", "07 Please Please Me", "08 Love Me Do", "09 P.S. I Love You", "10 Baby It's You", "11 Do You Want To Know A Secret", "12 A Taste Of Honey", "13 There's A Place", "14 Twist And Shout"]}, {album: "Revolver", musicas: ["01 Taxman", "02 Eleanor Rigby", "03 I'm Only Sleeping", "04 Love You To", "05 Here, There And Everywhere", "06 Yellow Submarine", "07 She Said She Said", "08 Good Day Sunshine", "09 And Your Bird Can Sing", "10 For No One", "11 Doctor Robert", "12 I Want To Tell You", "13 Got To Get You Into My Life", "14 Tomorrow Never Knows"]}, {album: "Rubber Soul", musicas: ["01 Drive My Car", "02 Norwegian Wood (The Bird Has Flown)", "03 You Won't See Me", "04 Nowhere Man", "05 Think For Yourself", "06 The Word", "07 Michelle", "08 What Goes On", "09 Girl", "10 I'm Looking Through You", "11 In My Life", "12 Wait", "13 If I Needed Someone", "14 Run For Your Life"]}, {album: "Sgt. Pepper's Lonely Hearts Club Band", musicas: ["01 Sgt Peppers Lonely Hearts Club Band", "02 With A Little Help From My Friends", "03 Lucy In The Sky With Diamonds", "04 Getting Better", "05 Fixing A Hole", "06 She's Leaving Home", "07 Being For The Benefit Of Mr Kite", "08 Within You Without You", "09 When I'm Sixty-Four", "10 Lovely Rita", "11 Good Morning Good Morning", "12 Sgt Peppers..(Reprise)", "13 A Day In The Life"]}, {album: "The Beatles (White Album) [Disc 1]", musicas: ["01 Back In The U.S.S.R.", "02 Dear Prudence", "03 Glass Onion", "04 Ob-La-Di, Ob-La-Da", "05 Wild Honey Pie", "06 The Continuing Story Of Bungalow Bill", "07 While My Guitar Gently Weeps", "08 Happiness Is A Warm Gun", "09 Martha My Dear", "10 I'm So Tired", "11 Blackbird", "12 Piggies", "13 Rocky Raccoon", "14 Don't Pass Me By", "15 Why Don't We Do It In The Road_", "16 I Will", "17 Julia"]}, {album: "The Beatles (White Album) [Disc 2]", musicas: ["01 Birthday", "02 Yer Blues", "03 Mother Nature's Son", "04 Everybody's Got Something To Hide Except Me And My Monkey", "05 Sexy Sadie", "06 Helter Skelter", "07 Long, Long, Long", "08 Revolution 1", "09 Honey Pie", "10 Savoy Truffle", "11 Cry Baby Cry", "12 Revolution 9", "13 Good Night"]}, {album: "With The Beatles", musicas: ["01 It Won't Be Long", "02 All I've Got To Do", "03 All My Loving", "04 Don't Bother Me", "05 Little Me", "06 Till There Was You", "07 Please Mister Postman", "08 Roll Over Beethoven", "09 Hold Me Tight", "10 You Really Got A Hold On Me", "11 I Wanna Be Your Man", "12 Devil In Her Heart", "13 Not A Second Time", "14 Money (That's What I Want)"]}];
+
+// {album: "Yellow Submarine", musicas: ["01 Yellow Submarine", "02 Only A Northern Song", "03 All Together Now", "04 Hey Bulldog", "05 It's All Too Much", "06 All You Need Is Love", "07 Pepperland", "08 Sea Of Time", "09 Sea Of Holes", "10 Sea Of Monsters", "11 March Of The Meanies", "12 Pepperland Laid Waste", "13 Yellow Submarine In Pepperland"]}
+
+// {album: "Past Masters, Volume One", musicas: ["01 Love Me Do", "02 From Me To You", "03 Thank You Girl", "04 She Loves You", "05 I'll Get You", "06 I Want To Hold Your Hand", "07 This Boy", "08 Komm, Gib Mir Deine Hand", "09 Sie Liebt Dich", "10 Long Tall Sally", "11 I Call Your Name", "12 Slow Down", "13 Matchbox", "14 I Feel Fine", "15 She's A Woman", "16 Bad Boy", "17 Yes It Is", "18 I'm Down"]}
+
+// {album: "Past Masters, Volume Two", musicas: ["01 Day Tripper", "02 We Can Work It Out", "03 Paperback Writer", "04 Rain", "05 Lady Madonna", "06 The Inner Light", "07 Hey Jude", "08 Revolution", "09 Get Back", "10 Don't Let Me Down", "11 The Ballad Of John And Yoko", "12 Old Brown Shoe", "13 Across The Universe", "14 Let It Be", "15 You Know My Name (Look Up The Number)"]}
+
 const aleatorio = (min, max) => {
        return Math.floor(Math.random() * (Math.floor(max) - Math.ceil(min) + 1)) + Math.ceil(min);
 }
 const aleatorioLista = (lista, restricao = false) => {
-       let posicao;
-       let maxIter = lista.length;
-       let cont = 0;
-
-       if (!restricao) {
-              posicao = aleatorio(0, lista.length - 1);
-              return lista[posicao];
-       } else {
-              do {
-                     cont++;
-                     posicao = aleatorio(0, lista.length - 1);
-              } while (!elementoNaLista(lista[posicao].album, restricao) && cont < maxIter);
-              return lista[posicao];
-       }
+       posicao = aleatorio(0, lista.length - 1);
+       return lista[posicao];
 }
 
 const restricaoAlbuns = () => {
@@ -23,7 +19,7 @@ const restricaoAlbuns = () => {
        let checks = [... document.querySelectorAll('.check')];
        checks.forEach(check => {
               if (check.checked) {
-                     considerados.push(check.name);
+                     considerados.push(parseInt(check.id));
               }
        });
 
@@ -131,7 +127,7 @@ const btnSalvar = document.querySelector('#btnSalvar');
 let musica = document.querySelector('#musica');
 
 
-let musicaEscolhida, audio, tempoAleatorio;
+let albumEscolhido, musicaEscolhida, audio, tempoAleatorio;
 
 toggleToast.addEventListener('click', () => {
        if (toastAtivo) {
@@ -173,8 +169,10 @@ const geraMusica = () => {
 	let clone = templateAudio.content.cloneNode(true);
 	musica.appendChild(clone);
 	audio = document.querySelector('audio');
-	musicaEscolhida = aleatorioLista(musicasObj, albunsConsiderados);
-	audio.setAttribute('src', `musicas/${musicaEscolhida.titulo_mp3}.mp3`);
+       let posAlbum = aleatorioLista(albunsConsiderados);
+       albumEscolhido = novoDataset[posAlbum];
+	musicaEscolhida = aleatorioLista(albumEscolhido.musicas);
+	audio.setAttribute('src', `musicas/${musicaEscolhida}.mp3`);
 	audio.addEventListener('loadedmetadata', function() {
 		// loadedmetadata para esperar até que os metadados do arquivo de áudio sejam carregados
 		tempoAleatorio = aleatorio(0, audio.duration - 15);
@@ -288,23 +286,23 @@ const elementoNaLista = (elemento, lista) => {
 };
 
 const trataDicaTextual = () => {
-       if (dicasUsadas < limiteDicas && dicaAtual < 2) {
+       if (dicasUsadas < limiteDicas && dicaAtual < 1) {
               let li = document.createElement('li');
               let span = document.createElement('span');
               switch(dicaAtual) {
                      case 0:
                             pontuacaoAtual -= 1;
-                            span.textContent = `${musicaEscolhida.album} (${musicaEscolhida.ano})`;
+                            span.textContent = `${albumEscolhido.album}`;
                             li.textContent = `Do álbum `;
                             li.appendChild(span);
                             dicasSorteadas.push(0);
                             break;
                      case 1:
-                            pontuacaoAtual -= 1;
-                            span.textContent = `${musicaEscolhida.autores}`;
-                            li.textContent = `Composta por `;
-                            li.appendChild(span);
-                            break;
+                            // pontuacaoAtual -= 1;
+                            // span.textContent = `${musicaEscolhida.autores}`;
+                            // li.textContent = `Composta por `;
+                            // li.appendChild(span);
+                            // break;
               }
               dicas.style.display = 'block';
               dicas.appendChild(li);
@@ -349,30 +347,31 @@ btnSalvar.addEventListener('click', () => {
 });
 
 const trataEnvio = () => {
-
        // exibe as dicas no final, independente de ter acertado ou errado, para que o jogador tenha acesso a essa informação e possa aprender, caso já não saiba
        dicas.innerHTML = '';
        let li_1 = document.createElement('li');
        let span_1 = document.createElement('span');
-       span_1.textContent = `${musicaEscolhida.album} (${musicaEscolhida.ano})`;
+       span_1.textContent = `${albumEscolhido.album}`;
        li_1.textContent = `Do álbum `;
        li_1.appendChild(span_1);
        dicas.appendChild(li_1);
 
-       let li_2 = document.createElement('li');
-       let span_2 = document.createElement('span');
-       span_2.textContent = `${musicaEscolhida.autores}`;
-       li_2.textContent = `Composta por `;
-       li_2.appendChild(span_2);
-       dicas.appendChild(li_2);
+       // let li_2 = document.createElement('li');
+       // let span_2 = document.createElement('span');
+       // span_2.textContent = `${musicaEscolhida.autores}`;
+       // li_2.textContent = `Composta por `;
+       // li_2.appendChild(span_2);
+       // dicas.appendChild(li_2);
 
        dicas.style.display = 'block';
 
        resposta.classList.remove('borrado');
        respostaDada = document.getElementById('respostaUsuario').value;
-       if (respostaDada === musicaEscolhida.titulo_normal && !errou) {
+
+       let nomeNormalizado = musicaEscolhida.slice(3);
+       if (respostaDada === nomeNormalizado && !errou) {
               pontuacao += pontuacaoAtual;
-              resposta.textContent = musicaEscolhida.titulo_normal;
+              resposta.textContent = nomeNormalizado;
               incremento = 10;
               streak++;
               switch(streak) {
@@ -407,7 +406,7 @@ const trataEnvio = () => {
        } else {
               errou = true;
               exibeToast('Oops! Você errou! Continue tentando.', 'brown');
-              resposta.textContent = musicaEscolhida.titulo_normal;
+              resposta.textContent = nomeNormalizado;
               incremento = 10;
               streak = 0;
               pontuacaoAtual = 0;
